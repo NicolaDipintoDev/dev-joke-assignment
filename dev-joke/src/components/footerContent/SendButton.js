5a1dc5e69432d7e8417f2bb3d7457edede410f12
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 23:05:36 +0200

    add button

--- dev-joke/src/components/footerContent/SendButton.js
+++ dev-joke/src/components/footerContent/SendButton.js
@@ -4,7 +4,13 @@ import { SendOutlined } from '@ant-design/icons';
 
 const SendButton = () => {
     return <div className="sendButton">
-        sendButton
+        <Button
+            ghost
+            type="text"
+            icon={<SendOutlined className='sendIcon' />}
+            disabled={false}
+            onClick={() => console.log('sendButton clicked')}
+        />
     </div>
 }
 
