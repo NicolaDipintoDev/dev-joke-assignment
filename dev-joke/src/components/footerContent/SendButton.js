b4c7b1e191c7f175c05293a0fcdd75e50c7abcf7
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 23:14:37 +0200

    fix Sendbutton

--- dev-joke/src/components/footerContent/SendButton.js
+++ dev-joke/src/components/footerContent/SendButton.js
@@ -3,16 +3,17 @@ import { Button, Popover } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
 
 const SendButton = () => {
-    return <Popover content='Send message' trigger="hover">
-        <Button
-            ghost
-            type="text"
-            icon={<SendOutlined className='sendIcon' />}
-            disabled={false}
-            onClick={() => console.log('sendButton clicked')}
-        />
-    </Popover>
-
+    return <span className="sendButton">
+        <Popover content='Send message' trigger="hover">
+            <Button
+                ghost
+                type="text"
+                icon={<SendOutlined className='sendIcon' />}
+                disabled={false}
+                onClick={() => console.log('sendButton clicked')}
+            />
+        </Popover>
+    </span>
 }
 
 export default SendButton;
