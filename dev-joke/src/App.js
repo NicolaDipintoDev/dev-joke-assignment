852569f96f80ceb5856db181b4a3b81297f86ebf
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 19:49:15 +0200

    add getMessages

--- dev-joke/src/App.js
+++ dev-joke/src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 
 function App() {
 
+  const getMessages = joke => {
+    let 
+      switch(joke.type){
+        case 'single':
+          return getSingleMessages(joke.joke);
+        case 'twopart':
+          return getTwoPartMessages(joke.setup, joke.delivery);
+
+      }
+  }
+
   useEffect(() => {
     axios.get(`https://sv443.net/jokeapi/v2/joke/Programming`)
       .then(res => {
