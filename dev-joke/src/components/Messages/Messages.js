80b9cbe0b88674e362e156575c4fe0cfc148c665
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-14 00:15:53 +0200

    add Delay

--- dev-joke/src/components/Messages/Messages.js
+++ dev-joke/src/components/Messages/Messages.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Message from './Message';
 import Ban from './Ban'
+import Delay from '../Delay';
 
 const Messages = ({ setIsWriting, messages }) => {
     return <div>
-        {messages.map((_, index) => index < 3 ?
-            <Message index={index} setIsWriting={setIsWriting} text='messaggio' author='Mario' /> :
-            <Ban />)}
+        {messages.map((message, index) => {
+
+            return <Delay key={'Delay' + index} wait={message.delay}>
+                <Message index={index} setIsWriting={setIsWriting} text='messaggio' author='Mario' />
+            </ Delay>
+        })}
     </div>
 }
 
