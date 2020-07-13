a4aee76e121bc3e624ea36e036486cc027474e52
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 22:48:29 +0200

    fix space

--- dev-joke/src/components/Messages/Messages.js
+++ dev-joke/src/components/Messages/Messages.js
@@ -3,11 +3,11 @@ import Message from './Message';
 import Ban from './Ban'
 
 const Messages = () => {
-    return <>
+    return <div>
         {[1, 2, 3, 4].map((_, index) => index < 3 ? 
         <Message text='messaggio' author='You'/> :
         <Ban />)}
-    </>
+    </div>
 }
 
 export default Messages;
