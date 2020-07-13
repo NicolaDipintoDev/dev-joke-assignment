e40edd4acfc55f3a0c70a4a22c797efe2f237906
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 19:51:59 +0200

    add getSingleMessages

--- dev-joke/src/App.js
+++ dev-joke/src/App.js
@@ -5,14 +5,19 @@ import './App.css';
 function App() {
 
   const getMessages = joke => {
-    let 
       switch(joke.type){
         case 'single':
           return getSingleMessages(joke.joke);
         case 'twopart':
           return getTwoPartMessages(joke.setup, joke.delivery);
-
       }
+  }
+
+  const getSingleMessages = joke => {
+      return [{text: 'volete sentire una bellissima barzelletta?', author: 'Mario', delay: 3000},
+      {text: 'Vai spara', author: 'Luigi', delay: 6000},
+      {text: joke, author: 'Mario',delay: 9000},
+      {text: 'removed', delay: 9300},];
   }
 
   useEffect(() => {
