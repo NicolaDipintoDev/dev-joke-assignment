56a1db5ce0e6bfcd5ee463d00bbac9f14a453ed7
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 20:17:43 +0200

    add layout schema

--- dev-joke/src/layout/LayoutHome.js
+++ dev-joke/src/layout/LayoutHome.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Breadcrumb } from 'antd';
 
 const { Header, Content, Sider, Footer } = Layout;
 
 const LayoutHome = () => {
-   return <div>layout</div>
+   return <Layout className='layout'>
+   <Header className="header">
+     Header
+   </Header>
+
+   <Layout>
+     <Sider width={200} className="site-layout-background sider" >
+       Sider
+     </Sider>
+     <Layout className='contentWrapper'>
+       <Breadcrumb className='breadcrumb'>
+         <Breadcrumb.Item >
+         Breadcrumb item
+         </Breadcrumb.Item>
+       </Breadcrumb>
+       <Content
+         className="site-layout-background content"
+       >
+        Content
+       </Content>
+       <Footer className='footer'>
+         footer
+       </Footer>
+     </Layout>
+   </Layout>
+ </Layout>
 }
 
 export default LayoutHome;
