7528408644119f05d902e5f697a26113548ce7e1
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 23:07:29 +0200

    add Popover

--- dev-joke/src/components/footerContent/SendButton.js
+++ dev-joke/src/components/footerContent/SendButton.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Popover } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
 
 const SendButton = () => {
     return <div className="sendButton">
-        <Button
-            ghost
-            type="text"
-            icon={<SendOutlined className='sendIcon' />}
-            disabled={false}
-            onClick={() => console.log('sendButton clicked')}
-        />
+        <Popover content='Send message' trigger="hover">
+            <Button
+                ghost
+                type="text"
+                icon={<SendOutlined className='sendIcon' />}
+                disabled={false}
+                onClick={() => console.log('sendButton clicked')}
+            />
+        </Popover>
     </div>
 }
 
