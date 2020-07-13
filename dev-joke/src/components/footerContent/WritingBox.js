e7b03386c9a3d1320201b815a05bb6d08cc87e7f
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 22:59:02 +0200

    add Input component

--- dev-joke/src/components/footerContent/WritingBox.js
+++ dev-joke/src/components/footerContent/WritingBox.js
@@ -3,7 +3,12 @@ import { Input } from 'antd';
 
 const WritingBox = () => {
     return <div className="writingBox">
-        Footer content
+        <Input
+            className='writingBox'
+            placeholder="Write a message"
+            value=''
+            onChange={event => console.log(event.target.value)}
+        />
     </div>
 }
 
