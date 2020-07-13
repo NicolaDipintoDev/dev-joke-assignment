eb4f68b99c421aa2f344cdb30b49ab4f8d3c219e
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-14 00:20:00 +0200

    fix messages

--- dev-joke/src/components/Messages/Messages.js
+++ dev-joke/src/components/Messages/Messages.js
@@ -3,12 +3,16 @@ import Message from './Message';
 import Ban from './Ban'
 import Delay from '../Delay';
 
-const Messages = ({ setIsWriting, messages }) => {
+const Messages = ({ setIsWriting, messages, isWriting }) => {
     return <div>
         {messages.map((message, index) => {
 
             return <Delay key={'Delay' + index} wait={message.delay}>
+
+                {isWriting !== '' ? 
                 <Message index={index} setIsWriting={setIsWriting} text='messaggio' author='Mario' />
+                : <Ban />}
+               
             </ Delay>
         })}
     </div>
