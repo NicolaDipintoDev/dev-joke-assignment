e44c2fd554c1b35d1e1399eafc87ff063cdba65a
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 21:56:33 +0200

    add real content to  BreadcrumbContent

--- dev-joke/src/components/breadcrumb/BreadcrumbContent.js
+++ dev-joke/src/components/breadcrumb/BreadcrumbContent.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const BreadcrumbContent = () => {
     return <div className="breadcrumbContent">
-        BreadcrumbCOntent
+        <div style={{ textAlign: "left", backGroundColor: "white", marginTop: '10px' }}>
+            <h3>DevTeam </h3>
+        </div>
+        <h5 style={{ marginLeft: '5px' }}>
+            Mario is writing...
+        </h5>
     </div>
 }
 
