d42c76a21e8bef8db620069aab794b6e3d03004e
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 22:39:31 +0200

    render Ban

--- dev-joke/src/components/Messages/Messages.js
+++ dev-joke/src/components/Messages/Messages.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Message from './Message';
+import Ban from './Ban'
 
 const Messages = () => {
     return <>
-        {[1, 2, 3, 4].map(() => <Message text='messaggio' author='You'/>)}
+        {[1, 2, 3, 4].map((_, index) => index < 3 ? 
+        <Message text='messaggio' author='You'/> :
+        <Ban />)}
     </>
 }
 
