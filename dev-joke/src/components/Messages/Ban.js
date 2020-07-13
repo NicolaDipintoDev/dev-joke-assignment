11a3c1be8b381395ebbdd8f3b2262bef5dbcb70b
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 22:35:02 +0200

    create Ban

---
+++ dev-joke/src/components/Messages/Ban.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './messages.css';
+
+const Ban = ({text, author}) => {
+
+    return <div className='ban'>
+        Banned
+    </div >
+
+}
+
+export default Ban;
