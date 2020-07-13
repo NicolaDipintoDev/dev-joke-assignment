fa64241428c8ea5d4c4095657912b42956d19972
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-13 21:11:29 +0200

    reCreate SiderContent

---
+++ dev-joke/src/components/SiderContent/SiderContent.js
@@ -0,0 +1,9 @@
+import React from './node_modules/react';
+
+const SiderContent = () => {
+    return <div className="siderContent">
+        SiderContent
+    </div>
+}
+
+export default SiderContent;
